1d12bcc47d65b72cc8c8d87a46375c5e47fe1ff4
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-09-23 13:51:00 +0800

    Move style file opening into function

--- src/style.rs
+++ src/style.rs
@@ -3,7 +3,8 @@
 use anyhow::{anyhow, Result};
 use printpdf::Mm;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Lines};
+use std::iter::Enumerate;
 use std::num::ParseFloatError;
 use std::path::PathBuf;
 use std::vec::Vec;
@@ -226,11 +227,16 @@ pub(crate) enum Command {
     MiscBox(MiscBox),
 }
 
-pub(crate) fn read(path: PathBuf) -> Result<Vec<Command>> {
+type LineIterator = Enumerate<Lines<BufReader<File>>>;
+fn get_lines(path: PathBuf) -> std::io::Result<LineIterator> {
     let style_file = File::open(path)?;
     let reader = BufReader::new(style_file);
+    Ok(reader.lines().enumerate())
+}
+
+pub(crate) fn read(path: PathBuf) -> Result<Vec<Command>> {
     let mut items: Vec<Command> = Vec::new();
-    for (index, line) in reader.lines().enumerate() {
+    for (index, line) in get_lines(path)? {
         let line = line?;
         // Handle comments
         if line.starts_with('#') {
