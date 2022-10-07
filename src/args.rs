12ee28447849eeae2279c1e1b5a8fed7610fc2aa
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-10-07 16:00:17 +0800

    Upgrade clap

--- src/args.rs
+++ src/args.rs
@@ -3,19 +3,19 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser)]
-#[clap(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None)]
 /// The standard arguments for the CLI.
 pub(crate) struct Args {
     /// Path to the input file in YAML format.
-    #[clap(short, long, value_parser, default_value = "data.yaml")]
+    #[arg(short, long, default_value = "data.yaml")]
     pub(crate) input: PathBuf,
 
     /// Path to the styling file.
-    #[clap(short, long, value_parser, default_value = "style.txt")]
+    #[arg(short, long, default_value = "style.txt")]
     pub(crate) style: PathBuf,
 
     /// Path to output the final PDF file to.
-    #[clap(short, long, value_parser, default_value = "output.pdf")]
+    #[arg(short, long, default_value = "output.pdf")]
     pub(crate) output: PathBuf,
 }
 
